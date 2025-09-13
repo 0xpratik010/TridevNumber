a7a1c30d1010f56a4f9a5553374875ca67767d7e
pratik <pratikmm010@gmail.com>
2025-09-14 00:43:14 +0530

    Added in vite.config.ts for Redirection

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    historyApiFallback: true, // ensures all routes fallback to index.html
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
